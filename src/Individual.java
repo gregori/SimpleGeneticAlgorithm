39f916fb0b284581438662b9f45a7e5ebac1ce7a
Lucas Gabriel Pinho <52325308+LucasGabrielPinho@users.noreply.github.com>
2022-04-19 18:06:35 -0300

    Update Individual.java

--- src/Individual.java
+++ src/Individual.java
@@ -1,14 +1,18 @@
 import java.util.Random;
 
 public class Individual {
-    int fitness = 0;
-    int[] genes = new int[5];
-    int geneLength = 5;
+    int fitness     = 0;
+    int[] genes     = new int[7];
+    int geneLength  = 7;
+    String[]names   = {"canivete", "feijao", "batatas", "lanterna", "saco de dormir", "corda", "bussola"};
+    int[] points    = {10, 20, 15, 2, 30, 10, 30};
+    int[] weight    = {1, 5, 10, 1, 7, 5, 1};
+    int pesototal   = 0;
 
     public Individual() {
         Random rn = new Random();
 
-        //Set genes randomly for each individual
+        //Definir genes aleatoriamente para cada indivíduo
         for (int i = 0; i < genes.length; i++) {
             genes[i] = Math.abs(rn.nextInt() % 2);
         }
@@ -16,15 +20,32 @@ public class Individual {
         fitness = 0;
     }
 
-    //Calculate fitness
-    public void calcFitness() {
+    //Calcular aptidão
+    public void calcFitness(){
+        fitness=0;
+        int peso=0;
 
-        fitness = 0;
-        for (int i = 0; i < genes.length; i++) {
-            if (genes[i] == 1) {
-                ++fitness;
+        for (int i = 0; i < names.length; i++) {
+            if( genes[i] != 0){
+                fitness += points[i];
+                peso += weight[i];
             }
+        }
+        if (peso>15){
+            fitness=0;
         }
     }
 
+    @Override
+    public String toString() {
+        String str = "";
+        for (int i = 0; i < geneLength; i++) {
+            str += names[i] + ": " + genes[i] + "\n";
+            if(genes[i] ==1 ){
+                pesototal += weight[i];
+            }
+        }
+        str += "fitness :" + fitness + "\n" + "peso total: " + pesototal + "\n";
+        return str;
+    }
 }
